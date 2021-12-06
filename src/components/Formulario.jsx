33411052ca61d09096c293263923ec71ad7cae8c
Jhofran Briceño <jhofran.dev@outlook.com>
2021-12-06 15:38:14 -0500

    0.1.3

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,76 @@
-
-
-
 const Formulario = () => {
   return (
-    <div className="">
-      <h2>Desde formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añadir Pacientes y {''}
+        <span className="text-indigo-600 font-bold text-lg">Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input 
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Propietario
+          </label>
+          <input 
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input 
+            id="email"
+            type="email"
+            placeholder="Email contacto propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+          <input 
+            id="Alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+          />
+        </div>
+
+      </form>
     </div>
   )
 }
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,9 +1,9 @@
-
-
-
 const ListadoPacientes = () => {
   return (
+    <div className="md:w-1/2 lg:w-3/5">
       <h2>Listado de pacientes</h2>
+
+    </div>
   )
 }
 
