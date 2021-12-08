d85bae95c4c17373f4074b00c2c3b6217e94e92d
Jhofran Briceño <jhofran.dev@outlook.com>
2021-12-08 11:05:17 -0500

    0.1.4

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,80 @@
-const Formulario = () => {
+import { useState, useEffect } from 'react'
+import Error from './Error'
+
+
+const Formulario = ({ setPacientes, pacientes, paciente, setPaciente }) => {
+  const [nombre, setNombre] = useState('')
+  const [propietario, setPropietario] = useState('')
+  const [email, setEmail] = useState('')
+  const [fecha, setFecha] = useState('')
+  const [sintoma, setSintoma] = useState('')
+
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    if( Object.keys(paciente).length > 0 ) {
+      setNombre(paciente.nombre)
+      setPropietario(paciente.propietario)
+      setEmail(paciente.email)
+      setFecha(paciente.fecha)
+      setSintoma(paciente.sintoma)
+
+    }
+  }, [paciente])
+
+  const generarId = () => {
+    const random = Math.random().toString(36).substr(2)
+    const fecha = Date.now().toString(36)
+
+    return random + fecha
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // Validación del formulario
+    if( [nombre, propietario, email, fecha, sintoma].includes('') ) {
+      setError(true)
+      return
+    }
+
+    setError(false)
+
+    // Objeto de paciente
+    const objetoPaciente = {
+      nombre, 
+      propietario, 
+      email, 
+      fecha, 
+      sintoma,
+    }
+
+    if(paciente.id) {
+      // Editando
+      objetoPaciente.id = paciente.id
+
+      const pacientesActualizados = pacientes.map( pacienteState => pacienteState.id === paciente.id ? objetoPaciente : pacienteState)
+
+      setPacientes(pacientesActualizados)
+      setPaciente({})
+
+    } else {
+      // nuevo registro
+      objetoPaciente.id = generarId()
+      setPacientes([...pacientes, objetoPaciente])
+    }
+
+
+    // Reiniciar el form
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintoma('')
+  }
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-3">
 
       <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
 
@@ -9,7 +83,12 @@ const Formulario = () => {
         <span className="text-indigo-600 font-bold text-lg">Administralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+
+        { error && <Error mensaje='Todos los campos son obligatorios'/> }
 
         <div className="mb-5">
           <label
@@ -23,6 +102,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre de la mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+            value={nombre}
+            onChange={ (e) => setNombre(e.target.value) }
           />
         </div>
 
@@ -38,6 +119,8 @@ const Formulario = () => {
             type="text"
             placeholder="Nombre del propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+            value={propietario}
+            onChange={ (e) => setPropietario(e.target.value) }
           />
         </div>
 
@@ -53,6 +136,8 @@ const Formulario = () => {
             type="email"
             placeholder="Email contacto propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </div>
 
@@ -64,11 +149,35 @@ const Formulario = () => {
             Alta
           </label>
           <input 
-            id="Alta"
+            id="alta"
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+            value={fecha}
+            onChange={ (e) => setFecha(e.target.value) }
           />
         </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Síntomas
+          </label>
+          <textarea 
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-lg"
+            placeholder="Describe los Síntomas"
+            value={sintoma}
+            onChange={ (e) => setSintoma(e.target.value) }
+          />
+        </div>
+
+        <input 
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value={ paciente.id ? 'Editar Paciente' : 'Agregar Paciente'}
+        />
 
       </form>
     </div>
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,48 @@
-const ListadoPacientes = () => {
+import Paciente from "./Paciente"
+
+
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h2>Listado de pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+
+      {pacientes && pacientes.length ? (
+        <>
+
+          <h2 className="font-black text-3xl text-center">
+            Listado de pacientes
+          </h2>
+
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+
+          { pacientes.map( paciente => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+
+        </>
+      ) : (
+
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay Pacientes
+          </h2>
+
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+
+      )}
 
     </div>
   )
